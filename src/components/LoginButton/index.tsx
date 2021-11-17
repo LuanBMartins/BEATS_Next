fcb92dfac51517ed119b50580901b789b2d60bd6
Rodrigo Moreno <rmoreno.w@gmail.com>
2021-11-17 01:37:06 -0300

    :lipstick: Fix on Framer breaking Hover Button

--- src/components/LoginButton/index.tsx
+++ src/components/LoginButton/index.tsx
@@ -17,12 +17,14 @@ export function LoginButton() {
             <AnimatePresence initial={false}>
                 {!isModalOpen && (
                     <motion.button
-                        className='rounded-20px border-whiteBeats-900 border flex w-28 justify-center items-center faded-bg hover:opacity-80 outline-none'
+                        className='rounded-20px border-whiteBeats-900 border flex w-28 justify-center items-center faded-bg hover:opacity-40 outline-none'
                         onClick={controlModal}
                         initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
                         exit={{ opacity: 0 }}
                         transition={{ duration: 0.4 }}
+                        whileHover={{ opacity: 0.8 }}
+                        whileTap={{ opacity: 0.8 }}
                     >
                         <Image src='/Material Icons/person.svg' width={32} height={32} alt='Project Logo' />
                     </motion.button>
